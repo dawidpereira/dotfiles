6754efbabd0bbde1f8fc3efa830d0725d5be4791
Dawid Pereira <pereiradawid@outlook.com>
2026-03-15 20:49:04 +0100

    feat(nvim): add clipboard yank on Enter in notification picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,19 +1,30 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		picker = {
-			layouts = {
-				sidebar = {
-					layout = {
-						position = "right",
-					},
-				},
-			},
-		},
-		lazygit = {
-			env = {
-				SHELL = "/bin/zsh",
-			},
-		},
-	},
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        notifications = {
+          confirm = function(picker, item)
+            if item and item.item then
+              vim.fn.setreg("+", item.item.msg)
+              Snacks.notify("Copied to clipboard", { title = "Notifications" })
+            end
+            picker:close()
+          end,
+        },
+      },
+      layouts = {
+        sidebar = {
+          layout = {
+            position = "left",
+          },
+        },
+      },
+    },
+    lazygit = {
+      env = {
+        SHELL = "/bin/zsh",
+      },
+    },
+  },
 }
